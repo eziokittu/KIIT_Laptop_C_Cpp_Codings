9982d5e5a91a916422d76d02bdb1f19bf319c765
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2022-10-17 02:19:30 +0530

    Incomplete leetcode problem 486

--- Competitive Codings/LeetCode_486_PredictTheWinner.cpp
+++ Competitive Codings/LeetCode_486_PredictTheWinner.cpp
@@ -6,56 +6,114 @@
 
 using namespace std;
 
-int score1, score2;
+int score1=0, score2=0;
 
 // algo for optimal selection of turn by any player
 int WhatToSelect(vector<int> &nums){
-
-    int first, last, valueToReturn;
+    cout << "\n-------Debug 0------";
+    int first = nums[0], last = nums[nums.size()-1], valueToReturn=0;
     // when array length is less than 4
     if (nums.size()<4){
-        first = nums[0];
-        last = nums[nums.size()-1];
+        cout << "\n-------Debug 1------";
         if (first < last) {
+            cout << "\n-------Debug 1.1----";
             valueToReturn = last;
-            nums.erase(nums.end());
+            // nums.erase(nums.end());
+            nums.erase(nums.begin()+nums.size()-1);
         }
         else {
+            cout << "\n-------Debug 1.2----";
             valueToReturn = first;
             nums.erase(nums.begin());
         }
     }
     // when array length is 4 or more
     else if (nums.size()>=4){
-        int first_next = nums[1], last_previous = nums[nums.size()-1];
+        cout << "\n-------Debug 2------";
+        int first_next = nums[1], last_previous = nums[nums.size()-2];
         if (first + first_next > last + last_previous){
-            valueToReturn = last;
-            nums.erase(nums.end());
+            cout << "\n-------Debug 2.1----";
+            if (first + first_next >= (last + last_previous)*2){
+                if (first > first_next){
+                    valueToReturn = first;
+                    nums.erase(nums.begin());
+                }
+                else{
+                    valueToReturn = last;
+                    nums.erase(nums.begin()+nums.size()-1);
+                }
+            }
+            else {
+                valueToReturn = last;
+                // nums.erase(nums.end());
+                nums.erase(nums.begin()+nums.size()-1);
+            }
         }
-        else{
-            valueToReturn = first;
-            nums.erase(nums.begin());
+
+        // when a[0]+a[1] < a[n-1]+a[n-2]
+        else if (first + first_next < last + last_previous){
+            cout << "\n-------Debug 2.2----";
+            if ((first + first_next)*2 <= last + last_previous){
+                cout << "\n-------Debug 2.2.1--";
+                if (last > last_previous){
+                    cout << "\n-------Debug 2.2.1.1-valueToReturn="<<valueToReturn;
+                    valueToReturn = last;
+                    nums.erase(nums.begin()+nums.size()-1);
+                    cout << "\n-------Debug 2.2.1.2-valueToReturn="<<valueToReturn;
+                }
+                else {
+                    valueToReturn = first;
+                    nums.erase(nums.begin());
+                }
+            }
+            else {
+                cout << "\n-------Debug 2.2.2--";
+                valueToReturn = first;
+                // nums.erase(nums.end());
+                nums.erase(nums.begin());
+            }
+        }
+        else {
+            cout << "\n-------Debug 2.3----";
+            if (first < last) {
+                cout << "\n-------Debug 2.2.1----";
+                valueToReturn = last;
+                // nums.erase(nums.end());
+                nums.erase(nums.begin()+nums.size()-1);
+            }
+            else {
+                cout << "\n-------Debug 2.2.2----";
+                valueToReturn = first;
+                nums.erase(nums.begin());
+            }
         }
     }
 
+    cout << "\n-------Debug 3, valueToreturn="<<valueToReturn<<"------";
     return valueToReturn;
 }
 
 bool PredictTheWinner(vector<int>& nums) {
     
     bool player1Won = false;
-
+    int arraySize = nums.size();
     // iterating the array and getting the score for each player
-    for (int i=0; i<nums.size()-1; i++){
-
+    for (int i=0; i<arraySize; i++){
+        // Debugging
+        cout << "\n----Debugging to print the array----";
+        for (int i=0; i<nums.size(); i++){
+            cout << nums[i] << " ";
+        }
         // even iterations are for player 1 --- 0, 2, 4, 6, ...
         if (i%2==0){
             score1 += WhatToSelect(nums);
+            printf("\n--For i = %d, score1 = %d, score2 = %d --", i, score1, score2);
         }
 
         // odd iterations are for player 2 --- 1, 3, 5, 7, ...
         else if (i%2==1){
             score2 += WhatToSelect(nums);
+            printf("\n--For i = %d, score1 = %d, score2 = %d --", i, score1, score2);
         }
     }
 
@@ -65,10 +123,32 @@ bool PredictTheWinner(vector<int>& nums) {
 }
 
 int main(){
-    // vector<int> nums = {1,2,3,10,5,7,9,4};
-    vector<int> nums = {2,4,55,6,8};
+    // vector<int> nums = {2,4,55,1}; // correct
+    // vector<int> nums = {5,232,231,7}; // correct
+    // vector<int> nums = {10, 50, 5, 30}; // correct
+    // vector<int> nums = {5, 10, 500, 200}; // correct
+    vector<int> nums = {10,17,11,16,17,9,14,17,18,13,11,4,17,18,15,3,13,10};
+    // 10,10 - 10r
+    // 10,13 - 13
+    // 10, 3 -  3
+    // 10,15 - 10
+    // 17
+    // 11
+    // 15
+    // 16
+    // 17
+    // 9
+    // 14
+    // 18
+    // 17r
+    // 4
+    // 11
+    // 13
+    // 17
+    // 18
+    // p1 = 121 should win
 
-    cout <<endl<< PredictTheWinner(nums);
+    cout << endl << PredictTheWinner(nums);
 }
 
 /*
