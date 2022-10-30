c851fc2d11b0f6c26ac8293d94596767cb7140b0
Bodhisatta Bhattacharjee <52044555+eziokittu@users.noreply.github.com>
2022-10-30 20:33:57 +0530

    lab6_5 completed

--- KIIT_3rdSem_OOP_Lab/Assignment 6/lab6_5.cpp
+++ KIIT_3rdSem_OOP_Lab/Assignment 6/lab6_5.cpp
@@ -3,29 +3,72 @@
 // Find the area of each shape and display it.
 
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 class Shape{
     public:
-        void CalculateArea();
+        float area = 0;
 };
 
 class Circle: public Shape {
     public:
-        float radius; 
-        void 
+        float r; 
+        Circle (float _r){
+            r = _r;
+        }
+        void CalculateArea(){
+            area = r*r * 3.141;
+        }
+        void ShowArea(){
+            cout << "\nArea = "<<area;
+        }
 };
 
 class Triangle: public Shape {
     public:
         float a,b,c;
+        Triangle (float _a, float _b, float _c){
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+        void CalculateArea(){
+            float s = a+b+c;
+            area = (sqrt(s * (s-a) * (s-b) * (s-c)));
+        }
+        void ShowArea(){
+            cout << "\nArea = "<<area;
+        }
 };
 
 class Rectangle: public Shape {
     public:
         float h,b;
+        Rectangle(float _h, float _b){
+            h = _h;
+            b = _b;
+        }
+        void CalculateArea(){
+            area = h*b;
+        }
+        void ShowArea(){
+            cout << "\nArea = "<<area;
+        }
 };
 
 int main(){
+    Rectangle rect(10, 17);
+    Triangle tri(3, 4, 5);
+    Circle circ(10);
+    
+    rect.CalculateArea();
+    tri.CalculateArea();
+    circ.CalculateArea();
 
+    rect.ShowArea();
+    tri.ShowArea();
+    circ.ShowArea();
+
+    return 0;
 }
