4093fda43d123f192bff9f44d80eb79f1ac9a2a1
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2022-11-07 19:57:59 +0530

    linkedList_single is working as expected

--- KIIT_3rdSem_DSA/Practing DSA/linkedList_single.c
+++ KIIT_3rdSem_DSA/Practing DSA/linkedList_single.c
@@ -1,6 +1,5 @@
 // ----------------------------- SINGLE LINKED LIST -------------------------
-
-
+// Header files
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,22 +10,33 @@ struct node
     int data;
     struct node *next;
 };
-
 typedef struct node node;
-// declarations
+
+// function declarations
 void ReverseLinkedList();
 void createnode();
-int listLength(node *);
+int listLength();
 void InsertAtBegin();
 void InsertAtEnd();
 void InsertAtPos();
 void DeleteAtBegin();
 void DeleteAtEnd();
 
+// making the first node which points to address NULL
 node *head=NULL;
+// NOTE : All the functions defined, don't have the necessary head node as parameter.
+// Since head node is globally declared, we can access and modify the value of "head" 
+// directly from any function. Otherwise, if head was not defined or was defined
+// inside MAIN FUNCTION, then then we would vae to give the parameter like below ---
+// return_type funtion_name (node **head, other parameters if any ....) {
+//    .....
+// }
+// and we would call the fucntions as ---
+// function_name (&head, ...);
+// Note: we are passing the address of head pointer here.
 
 // function to get the length of the linked list
-int listLength(node *head){
+int listLength(){
     int count = 1;
     node *temp = head;
     if (temp== NULL) return 0;
@@ -44,8 +54,10 @@ void InsertAtBegin(){
     node *temp;
     temp=(node*)malloc(sizeof(node));
     if (temp==NULL){
+        printf("Memory Full! Cannot add node.");
         exit(1);
     }
+    printf("\n----- Insert node at begin -----\n");
     printf("Enter data element : ");
     scanf("%d",&temp->data);
     if (head==NULL) head=temp;
@@ -60,8 +72,13 @@ void InsertAtBegin(){
 void InsertAtEnd(){
     node* temp;
     temp = (node*) malloc (sizeof(node));
+    if (temp==NULL){
+        printf("Memory Full! Cannot add node.");
+        exit(1);
+    }
+    printf("\n----- Insert node at end -----\n");
     printf("Enter data element : ");
-    scanf("%d", temp->data);
+    scanf("%d", &temp->data);
     temp->next = NULL;
     if (head==NULL){
         head = temp;
@@ -85,11 +102,11 @@ void InsertAtPos(int pos){
         exit(1);
     }
     else if (pos==1) {
-        printf("InsertAtBegin(head) INVOKED\n");
+        printf("InsertAtBegin() INVOKED\n");
         InsertAtBegin(head);
     }
     else if (pos == length+1){
-        printf("InsertAtEnd(head) INVOKED\n");
+        printf("InsertAtEnd() INVOKED\n");
         InsertAtEnd(head);
     }
     else{
@@ -97,8 +114,10 @@ void InsertAtPos(int pos){
         int k=1;
         temp = (node*) malloc (sizeof(node));
         if (temp==NULL){
+            printf("Memory Full! Cannot add node.");
             exit(1);
         }
+        printf("\n----- Insert node at pos [%d] -----\n", pos);
         printf("Enter data : ");
         scanf("%d", &temp->data);
         temp->next = NULL;
@@ -119,8 +138,7 @@ void createnode()
     temp=(node*)malloc(sizeof(node));
     if (temp==NULL)
     {
-        /* code */
-        printf("Memory Full : ");
+        printf("\nCannot create Linked List! Memory Full!");
         exit(1);
     }
 
@@ -131,7 +149,6 @@ void createnode()
     // if there is no linked list, then head will have address of the first node
     if (head==NULL)
     {
-        /* code */
         head = temp;}
 
     else
@@ -139,7 +156,6 @@ void createnode()
         node *p=head;
         while (p->next !=NULL)
         {
-        /* code */
         p=p->next;
         }
         
@@ -150,7 +166,7 @@ void createnode()
 }
 
 // WAP to find middle of the linked list without using listLength()
-node* findMiddle_withoutListLength(){
+node* FindMiddle_withoutListLength(){
     node *slow=head, *fast=head;
     while (fast!=NULL && fast->next!=NULL && (fast->next)->next!=NULL){
         slow = slow->next;\
@@ -160,7 +176,7 @@ node* findMiddle_withoutListLength(){
 }
 
 // find the middle element of a single linked list
-node* findMiddle_usingListLength(){
+node* FindMiddle_usingListLength(){
     int length = listLength(head);
     int k=0, mid = length/2;
     node *p = head;
@@ -172,13 +188,14 @@ node* findMiddle_usingListLength(){
 }
 
 // WAP to find whether a cycle exist in singly linkedlist
-int findCycle(){
+void FindCycle(){
     node *slow=head, *fast=head;
+    printf("\n----- FindCycle() function called! -----\n");
     while (fast!=NULL && fast->next!=NULL && fast->next->next!=NULL){
         slow = slow->next;
         fast = fast->next->next;
         if (slow==fast){
-            printf("Cycle exits");
+            printf("Cycle exits.");
             exit(1);
         }
     }
@@ -192,6 +209,7 @@ void DeleteAtEnd(){
         printf("No Linked list exist so no node to delete");
         exit(1);
     }
+    printf("\n----- Deleting Node at END -----");
     // case when there is only 1 element in the linked list
     if (head->next==NULL){
         free(head);
@@ -215,6 +233,7 @@ void DeleteAtBegin(){
         printf("Deletion of node not possible. Linked list is empty.\n");
         exit(1);
     }
+    printf("\n----- Deleting Node at BEGIN -----");
     if (head->next==NULL){
         free(head);
         head=NULL;
@@ -234,7 +253,8 @@ void DeleteAtPos(int pos){
         printf("Invaid Position. ");
         exit(1);
     }
-    else if (pos==1) {
+    printf("\n----- Deleting Node at pos [%d] -----", pos);
+    if (pos==1) {
         printf("DeleteAtBegin(head) INVOKED\n");
         DeleteAtBegin(head);
     }
@@ -306,7 +326,7 @@ void PrintLinkedList(){
         printf("The linked List is empty.");
         exit(1);
     }
-    printf("\nPrinting the Linked List:\n");
+    printf("\nPrinting the Linked List with Current size [%d] :\n", listLength());
     node *p=head;
     do {
         printf("Address [%p] : data = [%d], next = [%p]\n", p, p->data, p->next);
@@ -318,16 +338,55 @@ void PrintLinkedList(){
 // driver code
 void main()
 {
-    int i,n;
-
-    // Creating the linked list
+    // Creating the linked list and printing
+    int n;
     printf("Enter number of nodes : ");
     scanf("%d",&n);
     printf("Total no. of nodes in linked list : %d\n",listLength(head));
-    for ( i = 0; i < n; i++) createnode();
-
-    // Printing
+    printf("\n------ Creating the Linked List ------\n");
+    for ( int i = 0; i < n; i++) createnode();
     PrintLinkedList();
 
     // Adding nodes and printing
+    InsertAtEnd();
+    PrintLinkedList();
+    InsertAtBegin();
+    PrintLinkedList();
+    printf("\nEnter position to insert new node: ");
+    int pos1; scanf ("%d", &pos1);
+    InsertAtPos(pos1); // at position 2
+    PrintLinkedList();
+
+    // printing middle element of linked list
+    printf("\n----- Finding Middle Element witout using ListLength() -----\n");
+    node *mid = FindMiddle_withoutListLength();
+    printf("Address [%p] : data = [%d], next = [%p]\n", mid, mid->data, mid->next);
+
+    // Deleting nodes and printing
+    DeleteAtEnd();
+    PrintLinkedList();
+    DeleteAtBegin();
+    PrintLinkedList();
+    printf("\nEnter position to delete node: ");
+    int pos2; scanf ("%d", &pos2);
+    DeleteAtPos(pos2);
+    PrintLinkedList();
+
+    // printing middle element of linked list
+    printf("\n----- Finding Middle Element using ListLength() -----\n");
+    mid = FindMiddle_usingListLength();
+    printf("Address [%p] : data = [%d], next = [%p]\n", mid, mid->data, mid->next);
+
+
+    // Using Find Cycle function
+    FindCycle();
+    node *p=head;
+    while (p->next!=NULL){
+        p = p->next;
+    } // p now holds the last node address, now we create the cycle
+    p->next = head;
+    free(p);
+    p=NULL;
+    printf("\n----- After creating the cycle -----");
+    FindCycle();
 }
