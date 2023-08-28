d7092a955a3319eff19acabb35169cc25833444b
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2023-08-28 19:08:08 +0530

    learnt how to sort 2D vectors

--- Competitive Codings/LeetCode_56_MergeIntervals.cpp
+++ Competitive Codings/LeetCode_56_MergeIntervals.cpp
@@ -1,49 +1,55 @@
 // LeetCode Problem 56 - Merge n
 // https://leetcode.com/problems/merge-n/
+
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-vector<vector<int>> merge(vector<vector<int>>& n) {
-    if (n.size()==1){
-        return n;
-    }
-    
-    // we sort the intervals based on starting interval
-    for (int i=0; i<n.size()-1; i++){
-        for (int j=i+1; j<n.size(); j++){
-            if (n[i][0]>n[j][0]){
-                vector<int> temp = n[i];
-                n[i] = n[j];
-                n[j] = temp;
-            }
-        }
-    }
-    // cout << "intervals are sorted!\n";
+// sort all the intervals based on left side
+// void sortIntervals(vector<vector<int>>& n) {
+//     int s = n.size();
+//     // classic bubble sorting
+//     for (int i=0; i<s-1; i++){
+//         for (int j=i+1; j<s; j++){
+//             // swapping
+//             if (n[j][0] < n[i][0]){
+//                 vector<int> temp = n[j];
+//                 n[j] = n[i];
+//                 n[i] = temp;
+//             }
+//         }
+//     }
+// }
 
-    // we minimize the intervals
-    vector<vector<int>> ans;
-    int start=n[0][0], end=n[0][1];
+vector<vector<int>> merge(vector<vector<int>>& n) {
+    // sortIntervals(n); // intervals are sorted
+
+    // copied this following sorting ... from stack overflow
+    sort(n.begin(), n.end(),
+        [](const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    });
+
+    vector<vector<int>> v;
+    v.push_back({n[0][0], n[0][1]});
+    
     for (int i=1; i<n.size(); i++){
-        if (end<n[i][0]){
-            ans.push_back({start,end});
-            start = n[i][0];
-            end = n[i][1];
+        int k=v.size()-1;
+        // checking the left bounds
+        if (n[i][0] <= v[k][1]){
+            // checking the right bounds
+            if (n[i][1] > v[k][1]){
+                v[k][1] = n[i][1];
+            }
         }
         else{
-            if (end<n[i][1]){
-                end=n[i][1];
-            }
+            v.push_back({n[i][0], n[i][1]});
         }
-        if (i==n.size()-1){
-            ans.push_back({start,end});
-            break;
-        }
-        // cout <<"for i="<<i<<", start="<<start<<", end="<<end<<endl;
     }
-    // cout <<"After: intervals size="<<ans.size()<<endl;
-    return ans;
+
+    return v;
 }
 
 int main(){
