d155ec6dca832e52c12553e6a241dce7feebf4ef
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2023-09-07 03:12:54 +0530

    gg

--- Competitive Codings/LeetCode_224_BasicCalculator1.cpp
+++ Competitive Codings/LeetCode_224_BasicCalculator1.cpp
@@ -2,85 +2,112 @@
 // https://leetcode.com/problems/basic-calculator/
 
 #include <iostream>
+#include <stack>
 using namespace std;
-int calculate(string str) {
-    str='0'+str;
-    string s="";
-    int i=0;
+
+// remove all spaces
+string removeSpaces(string s){
+    string str="";
+    for (char c: s){
+        if (c!=' '){
+            str+=c;
+        }
+    }
+    return str;
+}
+
+// calculate inside brackets
+void getResult(stack<int> &numbers, stack<char> &symbols){
+    while (numbers.size()>1 && symbols.size()>0) {
+        int a = numbers.top();
+        numbers.pop();
+        int b = numbers.top();
+        numbers.pop();
+        char c = symbols.top();
+        symbols.pop();
+
+        if (c=='+'){
+            numbers.push(b+a);
+        }
+        else if (c=='-'){
+            numbers.push(b-a);
+        }
+    }
+    if (symbols.top()=='('){
+        symbols.pop();
+    }
+}
+
+int calculate(string s) {
+    s = removeSpaces(s);
     bool isNegative=false;
-    while(true){
-        char c = str[i]-'0';
-        if (c>=0 && c<=9){
+    int i=0;
+
+    // to check if number starting is negative
+    if (s[0]=='-'){
+        i=1;
+        isNegative = true;
+    }
+
+    // declaring the stacks and temporary number
+    stack<int> numbers;
+    stack<char> symbols;
+    int num=0;
+
+    // iterating the string
+    for (; ; i++){ // s[i]!='\0'
+        char c = s[i];
+
+        // NUMBER
+        if ( (c-'0')>=0 && (c-'0')<=9 ){
+            num = (num*10) + (c-'0');
+        }
+
+        // SYMBOL
+        else{
+            // PUSH the previous number
+            numbers.push(num);
+            num=0; // refrest temporary number
+
+            // PUSH
+            if (c=='(' || c=='+' || c=='-'){
+                symbols.push(c);
+            }
+
+            // POP
+            else if (c==')'){
+                getResult(numbers, symbols);
+            }
+        }
+
+        // loop exit condition
+        // exceptional - last letter
+        if (s[i+1]=='\0'){
+            // PUSH the previous number
+            numbers.push(num);
+            num=0; 
+
+            // get the result
+            getResult(numbers, symbols);
+
             break;
         }
-        else {
-            if (str[i]=='-'){
-                isNegative=true;
-            }
-            i++;
-        }
+
+        cout << numbers.top()<< " ";
     }
-    for (i=i; i<str.size(); i++){
-        if (str[i]!=' ' && str[i]!=')' && str[i]!='('){
-            // std::cout<<"for str[i]="<<str[i]<<", s[j]="<<s[s.size()-1]<<endl;
-            if((str[i]=='-' && s[s.size()-1]=='-') ||
-                (str[i]=='+' && s[s.size()-1]=='+')) {
-                    // std::cout<<"------GG-------";
-                s[s.size()-1]='+';
-            }
-            else if ((str[i]=='-' && s[s.size()-1]=='+') ||
-                    (str[i]=='+' && s[s.size()-1]=='-')) {
-                    // std::cout<<"------GG-------";
-                s[s.size()-1]='-';
-            }
-            else 
-                s+=str[i];
-        }
-    }
-    // std::cout << "s= "<<s<<endl;
-    int num1=0,num2=0;
-    char op=' ';
-    bool num2Active=false;
-    for (char c:s){
-        // std::cout <<"num1="<<num1<<", num2="<<num2<<", op="<<(char)op<<endl;
-        if (c!='-' && c!='+'){
-            if (num2Active){
-                num2=num2*10+(c-'0');
-            }
-            else {
-                num1=num1*10+(c-'0');
-            }
-        }
-        else if ((op==' ') && (c=='-' || c=='+')){
-            op=c;
-            num2Active=true;
-        }
-        // else if ((op!=' ') && (c=='-' || c=='+')){
-        else {
-            if (op=='+'){
-                num1=num1+num2;
-            }
-            else if (op=='-'){
-                num1=num1-num2;
-            }
-            num2=0;
-            op=c;
-        }
-    }
-    if (op=='-')
-        num1=num1-num2;
-    else 
-        num1=num1+num2;
+    cout <<endl;
+
     if (isNegative){
-        return -num1;
+        return -(numbers.top());
     }
-    return num1;
+    return numbers.top();
 }
 
 int main(){
     // cout << calculate("1+1")<<endl;
-    // std::cout << calculate("1-(     -2)")<<endl;
-    std::cout << calculate("- (3 + (4 + 5))")<<endl;
-    std::cout << calculate("-2  +1")<<endl;
+    // cout << calculate("1-(     -2)")<<endl;
+    cout << calculate("2333333+2-4")<<endl;
+    // cout << calculate("- (3 + (4 + 5)          )")<<endl;
+    // cout << calculate("-2  +1")<<endl;
     // cout << calculate("1-   5+1")<<endl;
 }
