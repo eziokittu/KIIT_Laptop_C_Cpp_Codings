4787256183c06ad44cc531d8f501345ebbfb685f
Bodhisatta Bhattacharjee <52044555+eziokittu@users.noreply.github.com>
2023-11-14 23:10:15 +0530

    gg

--- TrialAndPractice/Practice/8_LeastCommonSubSequence.c
+++ TrialAndPractice/Practice/8_LeastCommonSubSequence.c
@@ -2,74 +2,64 @@
 #include <string.h>
 #include <limits.h>
 
-int max (int a, int b){
-    return (a>b ? a : b);
+int max_of (int a, int b){
+    return (a > b ? a : b);
 }
 
-// LCS using recursion without memory
-// Demerits -
-// Time Complexity --> O(2^n) --> exponential
-// overlapping of funtion calls take place 
+// LCS without Memorization
 int LCS_1(char x[], char y[], int i, int j) {
-    if (x[i]=='\0' || y[j]=='\0'){
+    if (x[i]==0 || y[j]==0){
+        // printf("--x[%d]=%c, x[%d]=%c, %d\n", i, x[i], j, y[j], (x[i]==0));
         return 0;
     }
-    else if (x[i]==y[j]){
-        return 1 + LCS_1(x,y, i+1, j+1);
-    }
-    else {
-        return max( LCS_1(x,y,i,j+1), LCS_1(x,y,i+1,j) );
+    if (x[i]==y[j]){
+        return 1+ LCS_1(x, y, i+1, j+1);
     }
 }
 
-// LCS using recursion and memory (Dynamic Programming)
-// uses a 2D matrix as memory
-// removes the overlapping function calls
-// Time Complexity --> O(n x m)
-
-int LCS_2(char x[], char y[], int i, int j, int m, int n, int z[m][n]) {
-    for (int i=0; i<m; i++){
-        z[i][0] = 0;
-    }
-    for (int i=0; i<n; i++){
-        z[0][i] = 0;
-    }
-
-    for (int i=1; i<m; i++){
-        for (int j=1; j<n; j++){
-            if (x[i] == y[j]){
+// LCS with memorization
+int LCS_2(char x[], char y[], int m, int n, int z[m+1][n+1]){
+    for (int i=0; i<m+1; i++){
+        for (int j=0; j<n+1; j++){
+            if(i==0 || j==0){
+                z[i][j] = 0;
+            }
+            else if (x[i-1]==y[j-1]){
                 z[i][j] = z[i-1][j-1] + 1;
             }
             else {
-                z[i][j] = max(z[i][j-1], z[i-1][j]);
+                z[i][j] = max_of(z[i-1][j], z[i][j-1]);
             }
         }
     }
-    
-    printf("\nPrinting the table :\n");
-    for (int i=0; i<m; i++){
-        for (int j=0; j<n; j++){
-            printf("%d ", z[i][j]);
+
+    printf("\nPrinting the table z:\n");
+    for(int i=0; i<m+1; i++){
+        for(int j=0; j<n+1; j++){
+            printf("%d\t", z[i][j]);
         }
         printf("\n");
     }
 
-    return z[m-1][n-1];
+    printf(" [returing: z[%d][%d]\n ", m, n);
+    return z[m][n];
 }
 
 int main() {
-    // char s1[10] = {'c', 'b', 'c', 'a', 'b'};
-    // char s2[10] = {'c', 'b', 'a', 'b', 'c', 'b'};
-    // char s1[10] = {'a', 'b', 'c', 'd', 'e', 'f'};
-    // char s2[10] = {'b', 'c', 'a', 'b', 'd', 'e', 'f'};
-    char s1[] = "apbadbk";
-    char s2[] = "abapak";
-    int m = strlen(s1);
-    int n = strlen(s2);
-    int z[m][n];
+    // char s1[20] = "ABCDBCAD";
+    // char s2[20] = "BBCDBCAD";
+    char s1[20], s2[20];
+    printf("Enter 1st string: ");
+    scanf("%[^\n]%*c", s1);
+    printf("Enter 2nd string: ");
+    scanf("%[^\n]%*c", s2);
 
-    printf("\nLCS using recursion, without memorisation = %d\n", LCS_1(s1, s2, 0,0));
-    printf("LCS using recursion, with memorisation (Dynamic Programming) = %d\n\n", LCS_2(s1, s2, 0,0, strlen(s1), strlen(s2), z));
+    int l1 = strlen(s1);
+    int l2 = strlen(s2);
+    int m[l1+1][l2+1];
+
+    printf("Max length of sub sequence (using LCS_1): %d\n", LCS_1(s1, s2, 0, 0));
+    printf("Max length of sub sequence (using LCS_2): %d\n", LCS_2(s1, s2, l1, l2, m));
 
     return 0;
 }
