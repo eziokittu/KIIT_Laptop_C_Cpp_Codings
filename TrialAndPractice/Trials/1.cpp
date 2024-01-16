199129ccd682102ee476b4a56aeeb4220edac668
Bodhisatta Bhattacharjee <bodhisatta1999@gmail.com>
2024-01-16 21:12:04 +0530

    LeetCode Problem 81 - Search in rotated sorted array 2

--- TrialAndPractice/Trials/1.cpp
+++ TrialAndPractice/Trials/1.cpp
@@ -1,76 +1,50 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-// brute force method
-void getAdjacencyMatrix(vector<vector<int>> &m, vector<vector<int>> &edges, int n){
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            for (auto edge: edges){
-                int u = edge[0], v = edge[1];
-                if (u==i && v==j){
-                    m[i][j] = 1;
-                }
-            }
+bool isPossible(int n, int f, int a, int b, vector<int> v){
+    int start = 0;
+    int cost = 0;
+    for (auto i : v){
+        int cost1 = (i-start)*a;
+        int cost2 = b;
+        // int temp = cost1>cost2 ? cost2 : cost1;
+        int temp = cost1 + cost2;
+        cost += temp;
+        cout << "cost = "<<cost<<endl;
+        if (cost >= f){
+            return false;
         }
+        start = i;
     }
+    if (cost >= f)
+        return false;
+    return true;
 }
 
-int main()
-{
-    // getting the number of vertices
-    int count_vertices;
-    cout << "Enter the number of vertices: ";
-    cin >> count_vertices;
-    
-    // getting the number of edges
-    int count_edges;
-    cout << "Enter the number of edges: ";
-    cin >> count_edges;
-    
-    cout << "\n--- NOTE: edges are numbered from 0 to "<<count_vertices-1<<"(included) ---\n\n";
-    
-    // getting the pairs of edges
-    vector<vector<int>> edges;
-    cout << "Enter the edges pairs (u and v):\n";
-    for(int i=0; i<count_edges; i++){
-        int u, v;
-        cout << "For edge["<<i+1<<"]: ";
-        cin >> u >> v;
-        edges.push_back({u, v});
-    }
-    
-    // declaring and defining the adjacency matrix 
-    vector<vector<int>> adjacencyMatrix;
-    vector<int> temp;
-    for (int i=0; i<count_vertices; i++){
-        temp.push_back(0); 
-    }
-    for (int i=0; i<count_vertices; i++){
-        adjacencyMatrix.push_back(temp); 
-    }
-
-    // calling the function toi get the adjacency matrix
-    getAdjacencyMatrix (adjacencyMatrix, edges, count_vertices);
-
-    // printing
-    cout <<"-   ";
-    for (int i=0; i<count_vertices; i++){
-        cout <<i<<" ";
-    }
-    cout<<endl<<"   ";
-    for (int i=0; i<count_vertices; i++){
-        cout <<"--";
-    }
-    cout<<endl<<0<<" | ";
-    for (int i=0; i<count_vertices; i++){
-        for (int j=0; j<count_vertices; j++){
-            cout << adjacencyMatrix[i][j]<< " ";   
+int main() {
+    int m;
+    cin >> m;
+    vector<string> ans;
+    for (int i=0; i<m; i++){
+        int n,f,a,b;
+        cin>>n>>f>>a>>b;
+        vector<int> v;
+        for (int j=0; j<n; j++){
+            int temp;
+            cin>>temp;
+            v.push_back(temp);
         }
-        if (i!=count_vertices-1)
-            cout << "\n"<<i+1<<" | ";
+        
+        int temp = isPossible(n,f,a,b, v);
+        if (temp==true)
+            ans.push_back("YES");
+        else
+            ans.push_back("NO");
     }
-
-    return 0;
+    for (auto i : ans){
+        cout << i << endl;
+    }
 }
