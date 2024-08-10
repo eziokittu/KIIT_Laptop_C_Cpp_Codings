224ad345151b27abb6d5a85d50f74f0fc0fbcbc4
Bodhisatta Bhattacharjee <52044555+eziokittu@users.noreply.github.com>
2024-08-10 08:04:55 +0530

    bfs and dfs done

--- TrialAndPractice/Practice/Graphs/graph1.cpp
+++ TrialAndPractice/Practice/Graphs/graph1.cpp
@@ -11,7 +11,7 @@ void bfs(int n, vector<int> edges[], int start){
   visited[start] = 1;
 
   cout << "Traversing the graph using BFS: ";
-  
+
   while (!q.empty()){
     int node = q.front();
     q.pop();
@@ -25,6 +25,17 @@ void bfs(int n, vector<int> edges[], int start){
     }
   }
   cout << endl;
+}
+
+void dfs(vector<int> edges[], vector<int> &visited, int start){
+  if (visited[start] == 1){
+    return;
+  }
+  cout << start << " ";
+  visited[start] = 1;
+  for (int i : edges[start]){
+    dfs(edges, visited, i);
+  }
 }
 
 void undirected_graphs() {
@@ -46,8 +57,21 @@ void undirected_graphs() {
   }
 
   bfs(n, edges, 1);
+  vector<int> visited (n+1, 0);
+  cout <<"The DFS traversal is: ";
+  dfs(edges, visited, 1);
 }
 
 int main() {
   undirected_graphs();
 }
+
+// Enter no. of nodes: 5
+// Enter no. of edges: 4
+// Enter the 4 edges:
+// Enter edge 1: 1 2
+// Enter edge 2: 1 3
+// Enter edge 3: 2 4
+// Enter edge 4: 3 5
+// Traversing the graph using BFS: 1 2 3 4 5
+// The DFS traversal is: 1 2 4 3 5
